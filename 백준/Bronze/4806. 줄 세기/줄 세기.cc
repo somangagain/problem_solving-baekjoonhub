40e6779a19f9888232bd31ed581018e774ca1d2a
resomang <somangagain@gmail.com>
2024-07-25 18:57:42 +0900

    [Bronze III] Title: 줄 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int S;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    string s;
+    while (getline(cin, s)) S++;
+    cout << S;
+
+    return 0;
+}
