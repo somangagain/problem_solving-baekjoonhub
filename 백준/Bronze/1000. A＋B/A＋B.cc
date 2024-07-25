3db7b099402d484513d9f274ff9687bb4bd1aba7
resomang <somangagain@gmail.com>
2024-07-25 18:27:47 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A, B, S;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    // input
+    cin >> A >> B;
+
+    // solution
+    S = A + B;
+
+    // output
+    cout << S;
+
+    return 0;
+}
