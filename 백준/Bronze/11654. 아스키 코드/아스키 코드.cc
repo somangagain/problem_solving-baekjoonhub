3d495c1bdc7a6825883e94d432abedb5c1298d0c
resomang <somangagain@gmail.com>
2024-07-26 19:06:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char X;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> X;
+    cout << (int)X;
+
+    return 0;
+}
