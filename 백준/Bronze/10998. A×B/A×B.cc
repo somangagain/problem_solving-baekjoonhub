0f7af4693e715454f5b01caa754dd6f58d63d241
resomang <somangagain@gmail.com>
2024-07-26 17:25:28 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A, B, S;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> A >> B;
+    S = A * B;
+    cout << S;
+	
+    return 0;
+}
