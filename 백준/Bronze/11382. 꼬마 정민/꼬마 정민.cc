bc243c78735eea59874a4bedfea123a57074e84d
resomang <somangagain@gmail.com>
2024-07-26 18:24:34 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long A, B, C, S;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> A >> B >> C;
+    S = A + B + C;
+    cout << S;
+
+    return 0;
+}
