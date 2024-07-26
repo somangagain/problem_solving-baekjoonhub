c029b1c219e55fb79e204d15581589686ab48bcd
resomang <somangagain@gmail.com>
2024-07-26 19:27:21 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    string s;
+    while (getline(cin, s)) {
+        cout << s << '\n';
+    }
+
+    return 0;
+}
