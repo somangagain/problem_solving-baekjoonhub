a72116d4aa4adc46c74c6da1cf74be7dc14af549
resomang <somangagain@gmail.com>
2024-07-26 19:07:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    string s; cin >> s;
+    cout << s.length();
+
+    return 0;
+}
