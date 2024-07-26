a18886850f1ebb3e64acc58abe5b7293e788a42e
resomang <somangagain@gmail.com>
2024-07-26 19:26:03 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    string s; cin >> s;
+    int i; cin >> i;
+
+    cout << s[i - 1];
+
+    return 0;
+}
