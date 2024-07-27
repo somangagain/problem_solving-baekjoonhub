8b5e98357e64641c42962f2c945e82e2766ec06e
resomang <somangagain@gmail.com>
2024-07-27 23:33:11 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -9,7 +9,7 @@ int main()
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     cin >> A >> B >> V;
-    S = min(1 + (V - A) / (A - B) + ((V - A) % (A - B) ? 1 : 0), V / (A - B) + (V % (A - B) ? 1 : 0));
+    S = 1 + (V - A) / (A - B) + ((V - A) % (A - B) ? 1 : 0);
 
     cout << S;
 
