7f1cfd6a9f3f7a435870b0e374158f525baa8f52
resomang <somangagain@gmail.com>
2024-07-29 23:07:54 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -6,19 +6,16 @@ int main()
 {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    vector<int> V;
     int m = 0;
     int s = 0;
     for (int i = 0; i < 9; i++) {
-        int x; cin >> x; V.push_back(x);
+        int x; cin >> x;
         if (m < x) {
-            s = i;
-            m = x;
+            s = i; m = x;
         }
     }
 
     cout << m << '\n' << (s+1);
-
 
     return 0;
 }
