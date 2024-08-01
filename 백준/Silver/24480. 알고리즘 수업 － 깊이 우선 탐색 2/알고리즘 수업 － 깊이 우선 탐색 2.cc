3afbd744ecd8f1e81cd01ca87bc2962df01686be
resomang <somangagain@gmail.com>
2024-08-01 23:23:11 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 108 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void dfs(int o, vector<int> &V, vector<priority_queue<int>> &N, int* c) {
+    V[o] = ++*c;
+    while (!N[o].empty()) {
+        int n = N[o].top(); N[o].pop();
+        if (!V[n]) dfs(n, V, N, c);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n, m, r; cin >> n >> m >> r;
+    vector<int> V(n+1);
+    vector<priority_queue<int>> N(n+1);
+
+    while (m--) { 
+        int x, y; cin >> x >> y;
+        N[x].push(y); N[y].push(x);
+    }
+    
+    int c = 0;
+    dfs(r, V, N, &c);
+
+    for (int i = 1; i <= n; i++) {
+        cout << V[i] << '\n';
+    }
+
+    return 0;
+}
