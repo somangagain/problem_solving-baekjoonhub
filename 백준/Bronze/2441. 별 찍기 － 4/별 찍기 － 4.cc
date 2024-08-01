bf8625cfb66aca86efd8bfd5b21457c062a79de3
resomang <somangagain@gmail.com>
2024-08-01 23:45:30 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin >> n;
+    for (int i = n; i >= 1; i--) {
+        for (int j = 0; j < n - i; j++) cout << ' ';
+        for (int j = 0; j < i; j++) cout << '*';
+        cout << '\n';
+    }
+
+    return 0;
+}
