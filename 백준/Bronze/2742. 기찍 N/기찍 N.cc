bbff02cdbd1b5a28e4b54ece8d9c81521f1eba86
resomang <somangagain@gmail.com>
2024-08-01 23:53:41 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin >> n;
+    for (; n > 0; n--) {
+        cout << n << '\n';
+    }
+
+    return 0;
+}
