35635a3218a58ac9552b66f723af593498da6273
resomang <somangagain@gmail.com>
2024-08-06 22:49:44 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int r1, s; cin >> r1 >> s;
+    cout << (2 * s - r1);
+
+    return 0;
+}
